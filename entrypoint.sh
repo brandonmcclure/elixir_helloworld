1e6e93d9be462710c4956bc255ae850718816d44
Brandon McClure <brandonmcclure89@gmail.com>
2022-09-04 22:38:00 -0600

    kiss for entrypoint get it running on docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
-#!/bin/sh
+#/bin/sh
 
-# Create the database as configured
-mix ecto.create
 
-mix phx.server
-
-(
-	mix ecto.create
-	rc=$?
-	if [[ $rc != 0 ]]; then echo "init.sh failed with code $rc" && kill "$(ps aux | pgrep 'mix' | awk '{print $2}')"; fi
-) \
-	&
-mix phx.server
+cd /src && mix ecto.create
+cd /src && mix phx.server
